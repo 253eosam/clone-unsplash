bcb64c3c004a1735b2ef80d7b7f7e26308957dac
Sungjun Lee <253eosam@gmail.com>
2023-10-09 21:28:51 +0900

    add recoil state manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import './globals.css'
-import type { Metadata } from 'next'
 import StyledComponentsRegistry from '@/lib/registry'
+import { RecoilRoot, } from 'recoil';
 import AppHeader from '@/components/AppHeader'
 
 import { Inter } from 'next/font/google'
@@ -8,10 +10,6 @@ import AppContent from '@/components/AppContent'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: '아름다운 무료 이미지 및 사진 | Unsplash',
-  description: "어떤 프로젝트를 위해서든 다운로드 및 사용할 수 있는 아름다운 무료 이미지 및 사진입니다. 어떤 로열티 프리 또는 스톡 사진보다 좋습니다.",
-}
 
 const layout: React.FC<any> = ({
   children,
@@ -23,10 +21,12 @@ const layout: React.FC<any> = ({
     <html>
     <body className={inter.className} style={{ position: 'relative' }}>
       <StyledComponentsRegistry>
+      <RecoilRoot>
         <AppHeader/>
         <AppContent>
           {children}
         </AppContent>
+        </RecoilRoot>
       </StyledComponentsRegistry>
     </body>
   </html>
